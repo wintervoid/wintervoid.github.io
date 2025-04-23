86d6ec7e7879ae585d7a55743bc9fb535896608f
wintervoid <archer.nobel@proton.me>
2025-04-23 17:09:22 -0600

    i hate git pt 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://wintervoid.github.io', // Replace with your GitHub username or custom domain
+  base: '/',
+});
